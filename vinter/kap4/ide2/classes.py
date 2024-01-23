f06eb8d96787ff299c624287da8f7b6838a57ea9
lolzieslol <e-vilbjo4@asvg.no>
2024-01-23 20:07:16 +0100

    tastaturstyring og klokke

--- vinter/kap4/ide2/classes.py
+++ vinter/kap4/ide2/classes.py
@@ -1,4 +1,6 @@
 import pygame as pg
+from pygame.locals import (K_w,K_a,K_s,K_d)
+
 class Ball:
     def __init__(self,radius,windowObject,x,y,speed_x,speed_y):
         
@@ -30,3 +32,16 @@ class Ball:
                 self.speed_y = -self.speed_y
         
             self.y += self.speed_y
+            
+    def move(self,key):
+        # flytter spilleren
+        
+        if key[K_w]:
+            self.y -= self.speed_y
+        if key[K_a]:
+            self.y += self.speed_y
+        if key[K_s]:
+            self.x -= self.speed_x
+        if key[K_d]:
+            self.x += self.speed_x
+            
--- vinter/kap4/ide2/main.py
+++ vinter/kap4/ide2/main.py
@@ -7,6 +7,8 @@ from classes import Ball as BallClass
 # Initializing pygame
 pygame.init()
 
+clock = pygame.time.Clock()
+
 # Creating a window to "draw" our content
 WINDOW_WIDTH = 500 #pixels
 WINDOW_HEIGHT = 500 #pixels
@@ -15,28 +17,34 @@ window = pygame.display.set_mode([WINDOW_WIDTH, WINDOW_HEIGHT])
 brus = pygame.image.load('brus.png') 
 
 # makes a Ball-object
-theBall : BallClass = BallClass(radius=20, windowObject=window,x=250, y=250, speed=0.1)
-
+theBall : BallClass = BallClass(radius=20, windowObject=window,x=250, y=250, speed_x=3, speed_y=3)
 
 # repeat until the window is closed
 continuing : bool = True
 while continuing:
+    
     # Checks if the user has closed the window every time any user input happens
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             continuing = False
 
+    pressed_keys = pygame.key.get_pressed()
+    
+    
     # Colors the background blue
     window.fill((10, 129, 210))
 
     
     # Draws and moves the ball
-    theBall.DrawSelf()
-    
-    theBall.blit(brus,(50,50))
+    theBall.DrawSelf()   
+    # theBall.blit(brus,(50,50))
+    theBall.MoveSelfAtAGivenSpeed(["x","y"])
+    theBall.move(pressed_keys)
 
     # Updates all visual content
     pygame.display.flip() #update
+    
+    clock.tick(60)
 
 # Quits pygame
 pygame.quit()
